d2049aaadd01821a08d4f353ab28c3c724ddda81
Shawn Yuan <ysh83737@hotmail.com>
2017-11-27 15:46:20 +0800

    实现了功能，webpack-dev-server将打包输出文件保存在内存（依赖包见package.json文件）

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!
+            这次测试成功！！
+         </div>
+      );
+   }
+}
+
+export default App;
