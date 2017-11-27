7df6aa1e3cbc5316575cb1ff682097fba9a82e13
Shawn Yuan <ysh83737@hotmail.com>
2017-11-27 16:46:46 +0800

    更改了项目文件夹，用自己模仿的代码代替了下载的代代码

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello, World!!!<br />
+        欢迎来到W3Cschool教程学习！！！
+        这是你最爱的网站，你将在这里学习到很多知识！<br />
+        很好，终于实现了，可是这个东西在内存里，怎么提取出来呢！
+      </div>
+    );
+  }
+}
+
+export default App;
